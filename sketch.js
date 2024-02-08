0e712975180ede7a02a75b21036b3656fa79b504
magarini <catrinamag@gmail.com>
2024-02-08 16:09:27 +0200

    stable state

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
-// Copyright (c) 2019 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-/* ===
-ml5 Example
-PoseNet example using p5.js
-=== */
 
 let video;
 let poseNet;
 let poses = [];
+
+//images
+let elements=[];
+let elementIndex=0;
+
+let scenes=[];
+let sceneIndex=0;
+
+let textHighlighter;
+
+//texts
+let thought;
 
 let thoughts = [
   "now feels like eternity",
@@ -18,8 +21,6 @@ let thoughts = [
   "a bug that allowed us to be",
   "a perfect bug",
 ];
-
-let thought;
 
 let poem = [
   "we turn our thoughts to the internet",
@@ -46,12 +47,10 @@ let poems = [poem, poem2,poem3];
 let message = "breaks into a thousand pieces",
   font,
   bounds, // holds x, y, w, h of the text's bounding box
-  fontsize = 32,
+  fontsize = 24,
   x,
   y; // x and y coordinates of the text
 
-let img;
-let img2;
 
 let poemPositionX;
 let poemPositionY;
@@ -60,8 +59,33 @@ let poemOnDisplay;
 
 function preload() {
   font = loadFont("Poppins-Medium.ttf");
-  img = loadImage("digideer.jpg");
-  img2 = loadImage("digidear/Paint Layer 11.PNG");
+
+  textHighlighter=loadImage("digidear/Paint Layer 9.PNG");
+
+  let element0=loadImage("digidear/iridescentweb1.jpg.PNG");
+  let element1=loadImage("digidear/laptop_PNG5932.png.PNG");
+  let element2=loadImage("digidear/Paint Layer 2.PNG");
+  let element3=loadImage("digidear/Paint Layer 3.PNG");
+  let element4=loadImage("digidear/Paint Layer 4.PNG");
+  let element5=loadImage("digidear/Paint Layer 5.PNG");
+  let element6=loadImage("digidear/Paint Layer 4.PNG");
+  let element7=loadImage("digidear/Paint Layer 6.PNG");
+  let element8=loadImage("digidear/Paint Layer 7.PNG");
+  let element9=loadImage("digidear/Paint Layer 8.PNG");
+  // to change
+  let element10=loadImage("digidear/Paint Layer 3.PNG");
+  let element11=loadImage("digidear/Paint Layer 10.PNG");
+  let element12=loadImage("digidear/Paint Layer 11.PNG");
+  let element13=loadImage("digidear/Paint Layer 11.PNG");
+
+
+  elements=[element0,element1,element2,element3,element4,element5,element6,element7,element8,element9,element10,element11,element12,element13];
+
+  let scene0=loadImage("desk.jpg");
+  let scene1=loadImage("digideer.jpg");
+  let scene2=loadImage("tumblr.jpg");
+  scenes=[scene0,scene1,scene2]
+
 }
 
 function setup() {
@@ -79,19 +103,32 @@ function setup() {
   // Hide the video element, and just show the canvas
   video.hide();
 
-  thought = random(thoughts);
 
-  //
+
+  //nose Interaction
+  thought = random(thoughts);
   textSize(fontsize);
   bounds = font.textBounds(thought, 0, 0, fontsize);
   x = width / 2 - bounds.w / 2;
   y = height / 2 - bounds.h / 2;
 
+  //poemPosition
   poemPositionX = width / 2;
   poemPositionY = height / 2;
-  setInterval(changePoemPosition, 5000);
-
   poemOnDisplay = poems[0];
+
+
+  //timeRenewal
+  setInterval(changePoemPosition, 8000);
+  setInterval(addelementture, 5000);
+  setInterval(restart,10000)
+
+
+
+//initial scene
+  imageMode(CENTER);
+  image(scenes[sceneIndex], width / 2, height / 2);
+
 }
 
 function modelReady() {
@@ -103,57 +140,63 @@ function mousePressed() {
 }
 
 function draw() {
-  // image(video, 0, 0, width, height);
-  // strokeWeight(2);
-  background(0);
 
-  // tint(255, 180);
   imageMode(CENTER);
-  
-  image(img, width / 2, height / 2);
-  //  image(img,width/3,height/2);
+ 
 
-  // For one pose only (use a for loop for multiple poses!)
-  if (poses.length > 0) {
-    let pose = poses[0].pose;
 
-    // Create a pink ellipse for the nose
-    fill(213, 0, 143);
-    let nose = pose["nose"];
-    ellipse(nose.x, nose.y, 20, 20);
 
-    // check if the mouse is inside the bounding box and tickle if so
-    if (
-      nose.x >= bounds.x &&
-      nose.x <= bounds.x + bounds.w &&
-      nose.y >= bounds.y &&
-      nose.y <= bounds.y + bounds.h
-    ) {
-      thought = random(thoughts);
-      imageMode(CENTER);
-
-      image(img2,  nose.x, height / 2);
-
-      x += random(-5, 5);
-      y += random(-5, 5);
-    }
-  }
-
-  fill(255);
-  // textAlign(CENTER);
-
-  text(thought, x, y);
-  bounds = font.textBounds(thought, x, y, fontsize);
-
-  //backgroundText
+  //time-based poem
   push();
   fill(120);
-  textSize(24);
+  textSize(18);
+  imageMode(CENTER);
+  image(textHighlighter, poemPositionX, poemPositionY);
 
   for (i = 0; i < poem.length; i++) {
-    text(poemOnDisplay[i], poemPositionX, poemPositionY + i * 28);
+    textAlign(CENTER);
+  text(poemOnDisplay[i], poemPositionX, poemPositionY + i * 24);
   }
   pop();
+
+  //adding elements
+  for (i=0; i<elementIndex; i++){
+  image(elements[elementIndex],width/2,height/2);
+  }
+
+
+    // For one pose only (use a for loop for multiple poses!)
+    if (poses.length > 0) {
+      let pose = poses[0].pose;
+  
+      // Create a pink ellipse for the nose
+      fill('#9fa5ba');
+      let nose = pose["nose"];
+      noStroke();
+      ellipse(nose.x, nose.y, 20, 20);
+  
+      // check if the mouse is inside the bounding box and tickle if so
+      if (
+        nose.x >= bounds.x &&
+        nose.x <= bounds.x + bounds.w &&
+        nose.y >= bounds.y &&
+        nose.y <= bounds.y + bounds.h
+      ) {
+        imageMode(CENTER);
+        image(elements[8],  nose.x, nose.y);
+        thought = random(thoughts);
+        x += random(-5, 5);
+        y += random(-5, 5);
+      }
+    }
+  
+    //interactive poem
+    fill('#5f6a8d');
+    image(textHighlighter,  x, y);
+    text(thought, x, y);
+    bounds = font.textBounds(thought, x, y, fontsize);
+
+
 }
 
 function changePoemPosition() {
@@ -161,6 +204,28 @@ function changePoemPosition() {
   poemPositionY = random(height);
   let newPoem = Math.floor(Math.random()*3);
   poemOnDisplay = poems[newPoem];
+}
+
+
+function addelementture(){
+  elementIndex++;
+  if (elementIndex>=elements.length){
+    elementIndex=0;
+  }
+}
+
+
+function restart(){
+  // clear()
+  image(scenes[sceneIndex], width / 2, height / 2);
+  sceneIndex++;
+
+  if (sceneIndex>=scenes.length){
+    sceneIndex=0;
+  }
+
+  console.log('restarted',scenes[sceneIndex])
+
 }
 
 function windowResized() {
